ebd965f27f510e3ecce82e40720ffcd387e893f0
Bin Li <libin_75@hotmail.com>
2016-08-08 22:42:38 -0500

    make code dry, move alert into growtree function

--- main.js
+++ main.js
@@ -1,35 +1,42 @@
 function growtree (objTree) {
 	//make tree parts
+    objTree.character = textElementChar.value;
+	objTree.height = textElementNum.value;
 	var treePiece = [objTree.character];
 	var treePiecePerLine = objTree.character;
 	var treeIncreasePerLine = objTree.character + objTree.character;
 	var treeSpace = [""];
 	var spacePerline = "";
-	for (var i = 1; i <=objTree.height; i++) {
-		treePiecePerLine += treeIncreasePerLine;
-		treePiece.push(treePiecePerLine);
-		//next line is an alternative way to generate treePiece
-		// treePiece[i] = treePiece[i-1] + treeIncreasePerLine;
-		spacePerline += "&nbsp";//for html to display space properly
-		treeSpace.unshift(spacePerline);
+	if (objTree.character === "" || objTree.height === "") {
+    	alert("Both fields must have a value!");
+    }
+    else {
+		for (var i = 1; i <=objTree.height; i++) {
+			treePiecePerLine += treeIncreasePerLine;
+			treePiece.push(treePiecePerLine);
+			//next line is an alternative way to generate treePiece
+			// treePiece[i] = treePiece[i-1] + treeIncreasePerLine;
+			spacePerline += "&nbsp";//for html to display space properly
+			treeSpace.unshift(spacePerline);
+		}
+		//print on html
+		var preElement = document.createElement("pre");
+		for (var i = 0; i < objTree.height; i++) {
+			var newDiv = document.createElement("div");
+			newDiv.innerHTML =  treeSpace[i] + treePiece[i];
+			divElement.appendChild(preElement).appendChild(newDiv);
+		}
+		//for console.log to display space properly
+		var treeSpaceInConsole = [""];
+		var spacePerlineInConsole = "";
+		for (var i = 1; i <=objTree.height; i++) {
+			spacePerlineInConsole += " ";
+			treeSpaceInConsole.unshift(spacePerlineInConsole);
+		}
+		for (var i = 0; i < objTree.height; i++) {	
+			console.log(treeSpaceInConsole[i] + treePiece[i]);
+		}//end of console display
 	}
-	//print on html
-	var preElement = document.createElement("pre");
-	for (var i = 0; i < objTree.height; i++) {
-		var newDiv = document.createElement("div");
-		newDiv.innerHTML =  treeSpace[i] + treePiece[i];
-		divElement.appendChild(preElement).appendChild(newDiv);
-	}
-	//for console.log to display space properly
-	var treeSpaceInConsole = [""];
-	var spacePerlineInConsole = "";
-	for (var i = 1; i <=objTree.height; i++) {
-		spacePerlineInConsole += " ";
-		treeSpaceInConsole.unshift(spacePerlineInConsole);
-	}
-	for (var i = 0; i < objTree.height; i++) {	
-		console.log(treeSpaceInConsole[i] + treePiece[i]);
-	}//end of console display
 } //end of growtree function
 
 function clearTextarea() {
@@ -53,38 +60,23 @@ tree.character = "";
 tree.height = "";
 
 // Assign a function to be executed when the button is clicked
-buttonGrowTree.addEventListener("click", function() {
-		if (tree.character === "" || tree.height === "") {
-    		alert("Both fields must have a value!");
-    	}
-    	else {
-        growtree(tree);
-	    }
-	}
+buttonGrowTree.addEventListener("click", function(event) {
+    growtree(tree);
+    }
 );
 buttonClear.addEventListener("click", clearTextarea);
 
 // handles text enter event
 textElementChar.addEventListener("keyup", function(event) {
     event.preventDefault();
-    tree.character = textElementChar.value;
     if (event.keyCode == 13) {
-    	if (tree.character === "" || tree.height === "") {
-    		alert("Both fields must have a value!");
-    	}else {
         growtree(tree);
-	    }
     }
 })
 
 textElementNum.addEventListener("keyup", function(event) {
     event.preventDefault();
-	tree.height = textElementNum.value;
     if (event.keyCode == 13) {
-    	if (tree.character === "" || tree.height === "") {
-    		alert("Both fields must have a value!");
-    	}else {
         growtree(tree);
-	    }
     }
 })
